efe507472e5d44e3bfb3c99661ecc6988462e24a
Alexadner Dadukin <st235@yandex.ru>
2016-12-17 22:47:35 +0300

    Health Kit first public release

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,21 +1,25 @@
 #include <iarduino_SensorPulse.h>
+#include <SoftwareSerial.h>
 
 int val;
 int LED = 13;
 
 iarduino_SensorPulse Pulse(A0,1);      
+SoftwareSerial BTserial(2, 3); // RX | TX
 
 void setup()
 {
-  Serial.begin(9600);
+  BTserial.begin(9600);
+  BTserial.write("AT+NAMEHealthKit(ProtoV0.1)");
+  
   Pulse.begin();   
 }
 
 void loop()
 { 
-  if (Serial.available())
+  if (BTserial.available())
   {
-    byte incomingByte = Serial.read();
+    byte incomingByte = BTserial.read();
 
     if (incomingByte == 1) digitalWrite(13, HIGH);
        //if (Pulse.check(ISP_VALID)==ISP_CONNECTED){
@@ -24,7 +28,7 @@ void loop()
        // Serial.println(-1);
        //}
 
-       Serial.print("{ \"key\": 5 }\n");
+       BTserial.print("{ \"key\": 5 }\n");
        delay(10);
   }
 }
